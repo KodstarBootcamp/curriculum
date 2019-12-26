161e5f2bbbe7664fb4288618e460201f0714118b
Sami Kuzey <sodemircaglar@gmail.com>
2019-12-26 09:56:28 +0100

    Update and rename ara_sinav_soru_4_1.py to ara_sinav_soru_11_cozumleri.py

---
+++ python/week4/ara_sinav_soru_11_cozumleri.py
@@ -0,0 +1,10 @@
+sayi = input("Sayi girin: ")
+sonuc = int(sayi) + int(sayi * 2) + int(sayi * 3)
+print(sonuc)
+----
+sayi = int(input("Sayi girin: "))
+sonuc = sayi + 11 * sayi + 111 * sayi
+print(sonuc)
+----
+sayi = int(input("Sayi girin: "))
+print(sayi * 123)
--- python/week4/ara_sinav_soru_4_1.py
+++
@@ -1,3 +0,0 @@
-sayi = input("Sayi girin: ")
-sonuc = int(sayi) + int(sayi * 2) + int(sayi * 3)
-print(sonuc)
